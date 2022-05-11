fd9fd171af7288ae9bd6d64a424f99ecf8a31b3e
Marcos Del Sol Vives <marcos@orca.pet>
2022-05-12 01:16:00 +0200

    Agregar archivos de proyecto.

---
+++ StandUpTimer/TimerForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace StandUpTimer
+{
+    partial class TimerForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResetButton.Location = new System.Drawing.Point(12, 78);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(181, 23);
+            this.ResetButton.TabIndex = 0;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLabel.Location = new System.Drawing.Point(14, 14);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(179, 61);
+            this.TimeLabel.TabIndex = 1;
+            this.TimeLabel.Text = "30:00";
+            this.TimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TimerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 113);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.ResetButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TimerForm";
+            this.Text = "Stand up timer";
+            this.Load += new System.EventHandler(this.TimerForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label TimeLabel;
+    }
+}
+
---
+++ StandUpTimer/TimerForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace StandUpTimer
+{
+    public partial class TimerForm : Form
+    {
+        private DateTime StartTime;
+        private readonly Timer UpdateTimer = new Timer();
+        private readonly Timer ResetButtonEnableTimer = new Timer();
+        private readonly TimeSpan STAND_UP_TIME = TimeSpan.FromSeconds(5);
+
+        public TimerForm()
+        {
+            InitializeComponent();
+
+            UpdateTimer.Interval = 100;
+            UpdateTimer.Tick += new EventHandler(OnUpdateTimer);
+
+            ResetButtonEnableTimer.Interval = 3000;
+            ResetButtonEnableTimer.Tick += new EventHandler(OnResetButtonEnableTimer);
+        }
+
+        private void TimerForm_Load(object sender, EventArgs e)
+        {
+            StartTime = DateTime.Now;
+            UpdateTimer.Start();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            StartTime = DateTime.Now;
+            TimeLabel.ForeColor = DefaultForeColor;
+            UpdateTimer.Start();
+        }
+
+        private void OnUpdateTimer(object sender, EventArgs e)
+        {
+            var diff = STAND_UP_TIME - (DateTime.Now - StartTime);
+
+            int totalSecs = (int)Math.Ceiling(diff.TotalSeconds);
+            if (totalSecs < 0)
+                totalSecs = 0;
+
+            int minutes = totalSecs / 60;
+            int secs = totalSecs % 60;
+
+            TimeLabel.Text = $"{minutes:00}:{secs:00}";
+
+            if (totalSecs == 0)
+            {
+                TimeLabel.ForeColor = Color.Red;
+                UpdateTimer.Stop();
+
+                // Disable briefly, to prevent the reset button from being accidentally pressed if the user was typing
+                ResetButton.Enabled = false;
+                ResetButtonEnableTimer.Start();
+
+                // Bring to foreground
+                Activate();
+
+                // Play tone to notify
+                var dingPlayer = new SoundPlayer(ProgramResources.ding);
+                dingPlayer.Play();
+            }
+        }
+
+        private void OnResetButtonEnableTimer(object sender, EventArgs e)
+        {
+            ResetButton.Enabled = true;
+            ResetButtonEnableTimer.Stop();
+        }
+    }
+}
